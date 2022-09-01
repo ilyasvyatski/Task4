49f8984be097b00debb9a179e1dfe2b0f5a284e5
Alex <63662620+AlexFoxD@users.noreply.github.com>
2022-09-01 11:42:15 +0300

    Initial Commit

---
+++ Task4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task4
+//
+//  Created by neoviso on 9/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
